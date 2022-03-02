b1c16def32b3657941e0e2fb6b09e96599779105
youyuxu <youyuxu126@126.com>
2022-03-02 14:49:03 +0800

    push commit 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello gi43! master");
         System.out.println("hello gi43! hot-fix");
         System.out.println("hello gi43! push-fix");
+        System.out.println("hello gi43! push-fix2");
     }
 }
